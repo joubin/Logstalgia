446b37e8bb64c185c29a887d256f9aecb71d627a
Joubin <joubin.j@gmail.com>
2013-10-22 01:00:30 -0700

    made some modifications and tested the code
    Now the code has been tested on both 10.8.5+ and 10.9 GM

--- installHelp.sh
+++ installHelp.sh
@@ -1,10 +1,8 @@
 # This script was made for logstalgia
 
-# This code requires macports.
-if [ "$(id -u)" != "0" ]; then
-    echo "This script must be run as root" 1>&2
-    exit 1
-fi
+# This code requires macports as well as the developer tools that come with xcode.
+
+#login to developer.apple.com and download the commandline tools and install them before running this script
 
 
 sudo port selfupdate
@@ -13,19 +11,22 @@ if [ $? -ne 0 ]
     echo "Looks like MacPort is not installed or is not in your path" 1>&2
     exit 1 
 fi
-
-sudo port install libsdl
-sudo port install libsdl-devel
-sudo port install ftgl
-sudo port install libsdl_image
-
-cd /tmp
-git clone https://github.com/acaudwell/Logstalgia.git
-if [ $? -ne 0 ]; then
-    echo "Looks like you dont have git" 1>&2
+g++ --version
+if [ $? -ne 0 ]
+    then
+    echo "Looks like developer tools are no installed. Go to developer.apple.coma dn install the developer tools and run this script again" 1>&2
     exit 1
 fi
-
-
-cd Logstalgia
+set -e
+sudo port install libsdl
+sudo port install libsdl_image
+sudo port install libsdl
+sudo port install ftgl
+sudo port install pkgconfig
+cd /tmp
+curl -O http://logstalgia.googlecode.com/files/logstalgia-1.0.3.tar.gz
+tar xvzf logstalgia-1.0.3.tar.gz 
+cd logstalgia-1.0.3 
 ./configure && make && sudo make install 
+
+cd /tmp && rm -rf *ogstalgia*
